b4bfab5067da555b4f2c476f1355e8f87fa062fc
GarfieldApp <garfieldapp@outlook.com>
2026-03-25 17:04:28 +0100

    fix: add default server config to open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,5 +1,12 @@
 import type { OpenNextConfig } from "@opennextjs/cloudflare";
 
-const config: OpenNextConfig = {};
+const config: OpenNextConfig = {
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+    },
+  },
+};
 
 export default config;
